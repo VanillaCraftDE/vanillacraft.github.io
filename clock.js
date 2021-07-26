5ecf7be6b4eb7411615028dc7390394032a0100f
VanillaCraftDE <75546957+VanillaCraftDE@users.noreply.github.com>
2021-07-26 18:43:01 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-        var theRealease = new Date("March 12, 2021 18:00:00").getTime();
+        var theRealease = new Date("July 30, 2021 18:00:00").getTime();
 
         var interval = setInterval(function () {
                 var d = theRealease - new Date().getTime();
@@ -11,12 +11,12 @@ window.addEventListener("load", function () {
 
                 document.querySelector(".loader").style.opacity = "0";
                 document.querySelector(".content").className = "content animated fadeInDownBig";
-                // If the count down is over, write some text
+                // If the count down is over, write some text 
                 if (d < 0) {
                         clearInterval(interval);
                         document.querySelector(".countdown").innerHTML = "";
-                        document.getElementById("title").innerHTML = 'LET'S GO!';
-                        document.querySelector(".finished").innerHTML = 'Jetzt joinen! <span id="ip">MC.IP.COM</span>';
+                        document.getElementById("title").innerHTML = 'VanillaCraft Staffel3 ist offiziell gestartet!';
+                        document.querySelector(".finished").innerHTML = 'Die DynMap sollte gleich verfügbar sein.';
                 }
         }, 1000);
 });
